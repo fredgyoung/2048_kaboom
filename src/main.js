e3c209955853fccdde8538da1921596ed50f8b3e
fredgyoung <fredgyoung@gmail.com>
2023-12-25 00:40:21 -0500

    changes

--- src/main.js
+++ src/main.js
@@ -22,6 +22,26 @@ let board = [
 	[0,0,0,0],
 ]
 
+let coordinates = [
+	[[450, 110], [450, 210], [450, 310], [450, 410]],
+	[[550, 110], [550, 210], [550, 310], [550, 410]],
+	[[650, 110], [650, 210], [650, 310], [650, 410]],
+	[[750, 110], [750, 210], [750, 310], [750, 410]]
+]
+
+/*
+for (let y = 0; y < 4; y++) {
+	for (let x = 0; x < 4; x++) {
+		add([
+			pos(450 + x * 100, 110 + y * 100),
+			rect(80, 80),
+			color(238, 228, 218),
+			area(),
+		])
+	}
+}	
+*/
+
 // with options
 add([
 	pos(40, 40),
@@ -57,14 +77,14 @@ function drawGrid() {
 }
 
 function drawTiles() {
-	for (let y=0; y<4; y++) {
-		for (let x=0; x<4; x++) {
-			if (Math.floor(Math.random() * 2) == 0) {
+	for (let row=0; row<4; row++) {
+		for (let col=0; col<4; col++) {
+			if (board[row][col] > 0) {
 				add([
 					sprite("two"),
 					scale(1),
 					anchor("center"),
-					pos(490 + x * 100,  150 + y * 100),
+					pos(490 + col * 100,  150 + row * 100),
 					area(),
 				]);			
 			}
@@ -87,18 +107,6 @@ function mergeSimilarNumbers(arr) {
 		}
 	}
 	return arr;
-}
-
-// Rotate matrix 90, 180, or 270 degrees 
-function rotate4x4MatrixRight(oldArray, times) {
-	newArray = Array(4).fill().map(() => Array(4).fill(0));
-	// new col = 3 - old row
-	// new row = old col
-	for (let row=0; row<4; row++) {
-		for (let col=0; row<4; row++) {
-			newArray[row][col] = oldArray[col][3-row]
-		}	
-	}
 }
 
 // Move functions
@@ -163,14 +171,14 @@ onKeyPress("down", () => {
 
 onKeyPress("left", () => {
 	moveLeft();
+	printBoard()
+	drawGrid();
+	drawTiles();
 });
 
 onKeyPress("right", () => {
 	moveRight();
 });
-
-drawGrid();
-drawTiles();
 
 function boardFull() {
 	for (let row=0; row<4; row++) {
@@ -206,10 +214,10 @@ function addRandomTile() {
 	}
 }
 
-printBoard()
-
-while (!boardFull()) {
+for (let i=0; i<5; i++) {
 	addRandomTile();
-	printBoard()
 }
+printBoard()
+drawGrid();
+drawTiles();
 
